f7174608c838b06f17cdad7c4a9cecb2830cd67a
Athan Clark <athan.clark@gmail.com>
2016-04-07 19:24:18 -0600

    simple containers

--- README.md
+++ README.md
@@ -1,31 +1,4 @@
 # unfoldable-restricted
 
-TODO: Write description here
-
-## Installation
-
-TODO: Write installation instructions here
-
-## Usage
-
-### Creating `x`
-
-TODO: Write usage instructions here
-
-### Combining `x`
-
-TODO: Write usage instructions here
-
-### Consuming `x`
-
-TODO: Write usage instructions here
-
-## How to run tests
-
-```
-cabal configure --enable-tests && cabal build && cabal test
-```
-
-## Contributing
-
-TODO: Write contribution instructions here
+A pretty simple alternative for Unfoldable, such that
+we can add additional constraints to the contained elements.
--- src/Data/Unfoldable/Restricted.hs
+++ src/Data/Unfoldable/Restricted.hs
@@ -8,15 +8,65 @@
 
 module Data.Unfoldable.Restricted where
 
-import Data.Unfoldable      (Unfoldable (unfold))
-import Data.Unfolder        (Unfolder)
+import Data.Unfolder        (Unfolder (choose))
 import Data.Constraint      (Constraint)
 import Data.Constraint.Unit (Unit)
 
+import qualified Data.Set as Set
+import qualified Data.Map as Map
 
-class UnfoldableR (pred :: * -> Constraint) (t :: * -> *) | t -> pred where
+import qualified Data.HashMap.Lazy as HashMap
+import qualified Data.HashSet      as HashSet
+
+import Data.Hashable (Hashable)
+
+
+class UnfoldableR
+        (pred :: * -> Constraint)
+        (t :: * -> *)
+      | t -> pred where
   unfoldRestrict :: (pred a, Unfolder f) => f a -> f (t a)
 
-instance ( Unfoldable t
-         ) => UnfoldableR Unit t where
-  unfoldRestrict = unfold
+class BiUnfoldableR
+        (predA :: * -> Constraint)
+        (predB :: * -> Constraint)
+        (t :: * -> * -> *)
+      | t -> predA predB where
+  biunfoldRestrict :: (predA a, predB b, Unfolder f) => f a -> f b -> f (t a b)
+
+
+-- Containers
+
+instance UnfoldableR (Ord) Set.Set where
+  unfoldRestrict fa = choose
+    [ pure Set.empty
+    , Set.singleton <$> fa
+    , Set.union <$> unfoldRestrict fa <*> unfoldRestrict fa
+    ]
+
+instance BiUnfoldableR (Ord) Unit Map.Map where
+  biunfoldRestrict fa fb = choose
+    [ pure Map.empty
+    , Map.singleton <$> fa <*> fb
+    , Map.union <$> biunfoldRestrict fa fb <*> biunfoldRestrict fa fb
+    ]
+
+
+-- Unordered Containers
+
+
+class (Hashable a, Eq a) => Hashable' a
+
+instance UnfoldableR (Hashable') HashSet.HashSet where
+  unfoldRestrict fa = choose
+    [ pure HashSet.empty
+    , HashSet.singleton <$> fa
+    , HashSet.union <$> unfoldRestrict fa <*> unfoldRestrict fa
+    ]
+
+instance BiUnfoldableR (Hashable') (Unit) HashMap.HashMap where
+  biunfoldRestrict fa fb = choose
+    [ pure HashMap.empty
+    , HashMap.singleton <$> fa <*> fb
+    , HashMap.union <$> biunfoldRestrict fa fb <*> biunfoldRestrict fa fb
+    ]
