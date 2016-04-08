e505eae63b428f657770ddb583bc539bd3e0ea27
Athan Clark <athan.clark@gmail.com>
2016-04-08 14:19:36 -0600

    building up the rest of the api

--- src/Data/Unfoldable/Restricted.hs
+++ src/Data/Unfoldable/Restricted.hs
@@ -4,11 +4,12 @@
   , MultiParamTypeClasses
   , FunctionalDependencies
   , FlexibleInstances
+  , FlexibleContexts
   #-}
 
 module Data.Unfoldable.Restricted where
 
-import Data.Unfolder        (Unfolder (choose))
+import Data.Unfolder        (Unfolder (choose), ala, bfs)
 import Data.Constraint      (Constraint)
 import Data.Constraint.Unit (Unit)
 
@@ -17,15 +18,37 @@ import qualified Data.Map as Map
 
 import qualified Data.HashMap.Lazy as HashMap
 import qualified Data.HashSet      as HashSet
+import           Data.Hashable (Hashable)
 
-import Data.Hashable (Hashable)
+import           Data.Maybe (maybeToList, isNothing)
+import           Control.Monad.Trans.State
 
+
+-- * Unfoldable
 
 class UnfoldableR
         (pred :: * -> Constraint)
         (t :: * -> *)
       | t -> pred where
   unfoldRestrict :: (pred a, Unfolder f) => f a -> f (t a)
+
+unfoldRestrict_ :: (UnfoldableR Unit t, Unfolder f) => f (t ())
+unfoldRestrict_ = unfoldRestrict (pure ())
+
+unfoldRestrictBF :: (UnfoldableR p t, Unfolder f, p a) => f a -> f (t a)
+unfoldRestrictBF = ala bfs unfoldRestrict
+
+unfoldRestrictBF_ :: (UnfoldableR Unit t, Unfolder f) => f (t ())
+unfoldRestrictBF_ = bfs unfoldRestrict_
+
+unfoldrRestrict :: (UnfoldableR p t, p a) => (b -> Maybe (a, b)) -> b -> Maybe (t a)
+unfoldrRestrict f z = terminate . flip runStateT z . unfoldRestrictBF . StateT $ maybeToList . f
+  where
+    terminate [] = Nothing
+    terminate ((t, b):ts) = if isNothing (f b) then Just t else terminate ts
+
+
+
 
 class BiUnfoldableR
         (predA :: * -> Constraint)
